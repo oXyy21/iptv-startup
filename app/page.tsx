73464f151ca2f66174b20f83a697948a85e6611c
oXyy21 <nouamane2005@gmail.com>
2026-07-14 09:53:03 +0100

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
+import { Metadata } from "next"
 import { Button } from "@/components/ui/button"
+
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Welcome to the best IPTV Startup service.",
+  alternates: {
+    canonical: "/",
+  },
+}
 
 export default function Home() {
   return (
